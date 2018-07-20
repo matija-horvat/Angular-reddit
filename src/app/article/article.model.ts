b89e587ce0be4b9e816fb8837ac8ce2e8799f44b
Matt Horvat <mhorvaenator@gmail.com>
2018-07-20 19:53:29 +0200

    Initial commit

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,33 @@
+export class Article {
+    title: string;
+    votes: number;
+    link: string;
+
+    constructor(title: string, link: string, votes?: number) { 
+      
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+
+    }
+
+    
+  voteUp(): void{
+    this.votes +=1;
+  }
+
+  voteDown(): void{
+    this.votes -=1;
+  }
+
+  domain(): string{
+
+    try {
+        const domainAndPath: string = this.link.split('//')[1];
+        return domainAndPath.split('/')[0];
+    } catch (err) {
+        return null;
+    }
+  }
+
+}
